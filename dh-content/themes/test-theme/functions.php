39d00c751ad2afe127a229ccf60adcd5b7c813eb
David Hunter <d@vidhunter.scot>
2020-09-30 20:22:07 +0100

    Test Theme Tidy-Up
    * Migrated Contents of stylesheet.css to theme.css.
    - Removed stylesheet.css.
    * Migrated Header and Footer from functions file to index file.
    * Tidied Up Functions File

--- dh-content/themes/test-theme/functions.php
+++ dh-content/themes/test-theme/functions.php
@@ -1,8 +1,14 @@
 <?php
 
+theme_add_support( 'title-tag' );
+
+
+remove_action( 'theme_head', 'theme_meta_generator' );
+remove_action( 'theme_body_close', 'theme_attribution' );
+
+
 function testtheme_styles() {
 	theme_register_style( 'theme', theme_stylesheet_url(), filemtime( theme_file_path( 'theme', 'css' ) ) );
-	theme_register_style( 'stylesheet', theme_url( 'stylesheet.css' ), filemtime( theme_file_path( 'stylesheet', 'css' ) ) );
 }
 
 add_action( 'theme_head', 'testtheme_styles' );
@@ -13,24 +19,4 @@ function testtheme_scripts() {
 }
 
 add_action( 'theme_body_close', 'testtheme_scripts' );
-
-
-function testtheme_body_open() {
-	echo "<h1>" . config( 'name' ) . "</h1>";
-}
-
-add_action( 'theme_body_open', 'testtheme_body_open' );
-
-
-function testtheme_body_close() {
-	echo "<p>Copyright &copy; " . config( 'name' ) . " " . date('Y') . "</p>";
-}
-
-add_action( 'theme_body_close', 'testtheme_body_close' );
-
-remove_action( 'theme_head', 'theme_meta_generator' );
-remove_action( 'theme_body_close', 'theme_attribution' );
-
-
-theme_add_support( 'title-tag' );
 
--- dh-content/themes/test-theme/index.php
+++ dh-content/themes/test-theme/index.php
@@ -8,9 +8,13 @@
 	
 	<body>
 		<?php theme_body_top(); ?>
+
+		<h1><?php echo config( 'name' ); ?></h1>
 		
 		<p>This is the index file of <?php echo theme( 'name' ); ?> by <?php echo theme( 'author' ); ?>.</p>
 		
+		<p>Copyright &copy; <?php echo config( 'name' ) . " " . date('Y'); ?></p>
+
 		<?php theme_body_bottom(); ?>
 	</body>
 </html>
